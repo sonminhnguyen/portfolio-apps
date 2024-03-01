2809f15354c54d091e91aa1c5d197adaee5520dd
SonNguyen <sonbatgioi2@gmail.com>
2024-03-01 16:50:01 +0700

    test error no default engine by regenerate express-generator

--- app.js
+++ app.js
@@ -5,55 +5,48 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 // const weatherApp = require("./apps/weatherApp");
 const chatApp = require("./apps/chatApp");
 const noteApp = require("./apps/noteApp");
 const expensifyApp = require("./apps/expensifyApp");
 const stockApp = require("./apps/stockApp");
 
-
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 app.use(cors());
 app.use(logger('dev'));
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 // app.use("/weatherApp", weatherApp);
 // app.use("/chatApp", chatApp);
 app.use("/noteApp", noteApp);
 // app.use("/expensifyApp", expensifyApp);
 // app.use("/stockApp", stockApp);
 
-
-app.get('/', function(req, res, next) {
-  
-  res.send("resume page!");
-  // res.render('index', { title: 'Express' });
-});
-app.get('/*', function(req, res, next) {
-  
-  res.send("resume page!");
-  // res.render('index', { title: 'Express' });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
